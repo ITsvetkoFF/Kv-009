557dd35e27ec3a1de76e620ea7c7e1197a61d7bb
daniiltischenko <daniil.tischenko@gmail.com>
2015-06-25 01:03:02 +0300

    filled EcomapDBHelper.java

--- app/src/main/java/org/ecomap/android/app/data/EcoMapDBHelper.java
+++ app/src/main/java/org/ecomap/android/app/data/EcoMapDBHelper.java
@@ -1,9 +1,13 @@
 package org.ecomap.android.app.data;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import org.ecomap.android.app.data.EcoMapContract.PhotosEntry;
 import org.ecomap.android.app.data.EcoMapContract.ProblemsEntry;
+import org.ecomap.android.app.data.EcoMapContract.ResourcesEntry;
 
 /**
  * Manages a local database for weather data.
@@ -25,16 +29,49 @@ public class EcoMapDBHelper extends SQLiteOpenHelper {
 
         // Create a table to hold problems.
         final String SQL_CREATE_PROBLEMS_TABLE = "CREATE TABLE " + ProblemsEntry.TABLE_NAME + " (" +
-                ProblemsEntry._ID + " INTEGER PRIMARY KEY," +
-                ProblemsEntry.COLUMN_DATE + " TEXT NOT NULL " +
-                //add colums
+                ProblemsEntry._ID + " INTEGER PRIMARY KEY," + //TODO DO WE NEED AUTOINCREMENT?
+                ProblemsEntry.COLUMN_STATUS + " TEXT NOT NULL, " +
+                ProblemsEntry.COLUMN_TITLE + " TEXT NOT NULL, " +
+                ProblemsEntry.COLUMN_USER_NAME + " TEXT NOT NULL, " +
+                ProblemsEntry.COLUMN_PROBLEM_TYPE_ID + " INTEGER NOT NULL, " +
+                ProblemsEntry.COLUMN_SEVERITY + " INTEGER NOT NULL, " +
+                ProblemsEntry.COLUMN_VOTES_NUMBER + " INTEGER NOT NULL, " +
+                ProblemsEntry.COLUMN_DATE + " TEXT NOT NULL, " +
+                ProblemsEntry.COLUMN_CONTENT + " TEXT NOT NULL, " +
+                ProblemsEntry.COLUMN_LATITUDE + " TEXT NOT NULL, " +
+                ProblemsEntry.COLUMN_LONGTITUDE + " TEXT NOT NULL, " +
+                ProblemsEntry.COLUMN_PROPOSAL + " TEXT NOT NULL, " +
+                ProblemsEntry.COLUMN_REGION_ID + " INTEGER NOT NULL, " +
+                ProblemsEntry.COLUMN_COMMENTS_NUMBER + " INTEGER NOT NULL " +
+                " );";
+
+        final String SQL_CREATE_PHOTOS_TABLE = "CREATE TABLE " + PhotosEntry.TABLE_NAME + " (" +
+                PhotosEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                PhotosEntry.COLUMN_PROBLEM_ID + " INTEGER NOT NULL, " +
+                PhotosEntry.COLUMN_LINK + " TEXT NOT NULL, " +
+                PhotosEntry.COLUMN_CONTENT + " TEXT NOT NULL, " +
+                PhotosEntry.COLUMN_BLOB + " BLOB NOT NULL, " +
+
+                //setting up PROBLEM_ID as foreign key
+                " FOREIGN KEY (" + PhotosEntry.COLUMN_PROBLEM_ID + ") REFERENCES " +
+                ProblemsEntry.TABLE_NAME + " (" + ProblemsEntry._ID + " );";
+
+        final String SQL_CREATE_RESOURCES_TABLE = "CREATE TABLE " + ResourcesEntry.TABLE_NAME + " (" +
+                ResourcesEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                ResourcesEntry.COLUMN_TITLE + " TEXT NOT NULL, " +
+                ResourcesEntry.COLUMN_CONTENT + " TEXT NOT NULL" +
                 " );";
 
         sqLiteDatabase.execSQL(SQL_CREATE_PROBLEMS_TABLE);
+        sqLiteDatabase.execSQL(SQL_CREATE_PHOTOS_TABLE);
+        sqLiteDatabase.execSQL(SQL_CREATE_RESOURCES_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + ProblemsEntry.TABLE_NAME);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + PhotosEntry.TABLE_NAME);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + ResourcesEntry.TABLE_NAME);
 
     }
 }
