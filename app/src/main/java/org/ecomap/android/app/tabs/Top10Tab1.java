dd9a914bd7e2db824ab9fd46745d2c64bfe54d72
NICOLA <radakvitka>
2015-07-28 09:56:22 +0300

    top 10 tabs

--- app/src/main/java/org/ecomap/android/app/tabs/Top10Tab1.java
+++ app/src/main/java/org/ecomap/android/app/tabs/Top10Tab1.java
@@ -2,6 +2,7 @@ package org.ecomap.android.app.tabs;
 
 
 
+
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -10,13 +11,29 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import org.ecomap.android.app.R;
+import org.ecomap.android.app.data.EcoMapContract;
+import org.ecomap.android.app.data.model.ProblemPhotoEntry;
+import org.ecomap.android.app.sync.EcoMapAPIContract;
+import org.ecomap.android.app.sync.EcoMapService;
+import org.ecomap.android.app.widget.ExpandableHeightGridView;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.ecomap.android.app.data.EcoMapDBHelper;
+
 
 /**
  * Created by hp1 on 21-01-2015.
  */
-public class Top10Tab1 extends Fragment {
+public class Top10Tab1 extends Fragment implements View.OnClickListener {
+
+
 
     String [] top10pop = {"1. ТОП 10 популярних проблем","2. ТОП 10 популярних проблем","3. ТОП 10 популярних проблем","4. ТОП 10 популярних проблем","5. ТОП 10 популярних проблем","6. ТОП 10 популярних проблем","7. ТОП 10 популярних проблем","8. ТОП 10 популярних проблем","9. ТОП 10 популярних проблем","10. ТОП 10 популярних проблем"};
 
@@ -31,5 +48,12 @@ public class Top10Tab1 extends Fragment {
         lvTopPop.setAdapter(adapter1);
 
         return v;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+
+
     }
 }
