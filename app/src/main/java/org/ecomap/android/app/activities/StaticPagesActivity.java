ee9fd67ba79d592a0f60150839c5bf9f4ced1f1d
yuriysych <yuriy_sych@yahoo.com>
2015-08-05 02:37:58 +0300

    -

--- app/src/main/java/org/ecomap/android/app/activities/StaticPagesActivity.java
+++ app/src/main/java/org/ecomap/android/app/activities/StaticPagesActivity.java
@@ -3,6 +3,7 @@ package org.ecomap.android.app.activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
+import android.widget.TextView;
 
 import org.ecomap.android.app.R;
 
@@ -12,7 +13,9 @@ import org.ecomap.android.app.R;
 public class StaticPagesActivity extends AppCompatActivity {
 
     WebView webView;
+    TextView textView;
     String content;
+    String title;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,6 +23,10 @@ public class StaticPagesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_static_pages);
 
         content = getIntent().getStringExtra("content");
+        title = getIntent().getStringExtra("title");
+
+        textView = (TextView) findViewById(R.id.static_pages_item_title_inside);
+        textView.setText(title);
 
         webView = (WebView) findViewById(R.id.static_pages_item_content);
         webView.loadData(content, "text/html; charset=UTF-8", null);
