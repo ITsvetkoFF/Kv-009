9ab3d2aac644da87d0b7241999bac6f62ce145c8
yuriysych <yuriy_sych@yahoo.com>
2015-08-31 12:57:38 +0300

    -

--- app/src/main/java/org/ecomap/android/app/data/EcoMapDBHelper.java
+++ app/src/main/java/org/ecomap/android/app/data/EcoMapDBHelper.java
@@ -4,8 +4,6 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import org.ecomap.android.app.data.EcoMapContract.ProblemsEntry;
-import org.ecomap.android.app.data.EcoMapContract.ResourcesEntry;
 import org.ecomap.android.app.utils.SharedPreferencesHelper;
 
 /**
@@ -35,49 +33,58 @@ public class EcoMapDBHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
 
         // Create a table to hold problems.
-        final String SQL_CREATE_PROBLEMS_TABLE = "CREATE TABLE " + ProblemsEntry.TABLE_NAME + " (" +
-                ProblemsEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
-                ProblemsEntry.COLUMN_PROBLEM_ID + " INTEGER PRIMARY KEY," +
-                ProblemsEntry.COLUMN_STATUS + " TEXT NOT NULL, " +
-                ProblemsEntry.COLUMN_TITLE + " TEXT NOT NULL, " +
-                ProblemsEntry.COLUMN_USER_FIRST_NAME + " TEXT NOT NULL, " +
-                ProblemsEntry.COLUMN_USER_LAST_NAME + " TEXT NOT NULL, " +
-                ProblemsEntry.COLUMN_PROBLEM_TYPE_ID + " INTEGER NOT NULL, " +
-                ProblemsEntry.COLUMN_SEVERITY + " TEXT NOT NULL, " +
-                ProblemsEntry.COLUMN_NUMBER_OF_VOTES + " INTEGER NOT NULL, " +
-                ProblemsEntry.COLUMN_DATE + " TEXT NOT NULL, " +
-                ProblemsEntry.COLUMN_CONTENT + " TEXT NOT NULL, " +
-                ProblemsEntry.COLUMN_LATITUDE + " TEXT NOT NULL, " +
-                ProblemsEntry.COLUMN_LONGTITUDE + " TEXT NOT NULL, " +
-                ProblemsEntry.COLUMN_PROPOSAL + " TEXT NOT NULL, " +
-                ProblemsEntry.COLUMN_REGION_ID + " INTEGER NOT NULL, " +
-                ProblemsEntry.COLUMN_COMMENTS_NUMBER + " INTEGER NOT NULL, " +
-                ProblemsEntry.COLUMN_USER_ID + " INTEGER NOT NULL" +
+        final String SQL_CREATE_PROBLEMS_TABLE = "CREATE TABLE " + EcoMapContract.ProblemsEntry.TABLE_NAME + " (" +
+                EcoMapContract.ProblemsEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                EcoMapContract.ProblemsEntry.COLUMN_PROBLEM_ID + " INTEGER," +
+                EcoMapContract.ProblemsEntry.COLUMN_STATUS + " TEXT NOT NULL, " +
+                EcoMapContract.ProblemsEntry.COLUMN_TITLE + " TEXT NOT NULL, " +
+                EcoMapContract.ProblemsEntry.COLUMN_USER_FIRST_NAME + " TEXT NOT NULL, " +
+                EcoMapContract.ProblemsEntry.COLUMN_USER_LAST_NAME + " TEXT NOT NULL, " +
+                EcoMapContract.ProblemsEntry.COLUMN_PROBLEM_TYPE_ID + " INTEGER NOT NULL, " +
+                EcoMapContract.ProblemsEntry.COLUMN_SEVERITY + " TEXT NOT NULL, " +
+                EcoMapContract.ProblemsEntry.COLUMN_NUMBER_OF_VOTES + " INTEGER NOT NULL, " +
+                EcoMapContract.ProblemsEntry.COLUMN_DATE + " TEXT NOT NULL, " +
+                EcoMapContract.ProblemsEntry.COLUMN_CONTENT + " TEXT NOT NULL, " +
+                EcoMapContract.ProblemsEntry.COLUMN_LATITUDE + " TEXT NOT NULL, " +
+                EcoMapContract.ProblemsEntry.COLUMN_LONGTITUDE + " TEXT NOT NULL, " +
+                EcoMapContract.ProblemsEntry.COLUMN_PROPOSAL + " TEXT NOT NULL, " +
+                EcoMapContract.ProblemsEntry.COLUMN_REGION_ID + " INTEGER NOT NULL, " +
+                EcoMapContract.ProblemsEntry.COLUMN_COMMENTS_NUMBER + " INTEGER NOT NULL, " +
+                EcoMapContract.ProblemsEntry.COLUMN_USER_ID + " INTEGER NOT NULL" +
                 " );";
 
 
-        final String SQL_CREATE_RESOURCES_TABLE = "CREATE TABLE " + ResourcesEntry.TABLE_NAME + " (" +
-                ResourcesEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                ResourcesEntry.COLUMN_TITLE + " TEXT NOT NULL, " +
-                ResourcesEntry.COLUMN_CONTENT + " TEXT NOT NULL" +
+        final String SQL_CREATE_RESOURCES_TABLE = "CREATE TABLE " + EcoMapContract.ResourcesEntry.TABLE_NAME + " (" +
+                EcoMapContract.ResourcesEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                EcoMapContract.ResourcesEntry.COLUMN_TITLE + " TEXT NOT NULL, " +
+                EcoMapContract.ResourcesEntry.COLUMN_CONTENT + " TEXT NOT NULL" +
+                " );";
+
+        final String SQL_CREATE_PENDING_TABLE = "CREATE TABLE " + EcoMapContract.PendingProblemsEntry.TABLE_NAME + " (" +
+                EcoMapContract.PendingProblemsEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                EcoMapContract.PendingProblemsEntry.COLUMN_PROBLEM_ID + " INTEGER NOT NULL, " +
+                EcoMapContract.PendingProblemsEntry.COLUMN_PHOTOS + " TEXT" +
                 " );";
 
         sqLiteDatabase.execSQL(SQL_CREATE_PROBLEMS_TABLE);
         sqLiteDatabase.execSQL(SQL_CREATE_RESOURCES_TABLE);
+        sqLiteDatabase.execSQL(SQL_CREATE_PENDING_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
-        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + ProblemsEntry.TABLE_NAME);
-        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + ResourcesEntry.TABLE_NAME);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + EcoMapContract.ProblemsEntry.TABLE_NAME);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + EcoMapContract.ResourcesEntry.TABLE_NAME);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + EcoMapContract.PendingProblemsEntry.TABLE_NAME);
         onCreate(sqLiteDatabase);
         SharedPreferencesHelper.updateNumRevision(mContext, 0);
     }
 
     @Override
     public void onDowngrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
-        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + ProblemsEntry.TABLE_NAME);
-        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + ResourcesEntry.TABLE_NAME);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + EcoMapContract.ProblemsEntry.TABLE_NAME);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + EcoMapContract.ResourcesEntry.TABLE_NAME);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + EcoMapContract.PendingProblemsEntry.TABLE_NAME);
         onCreate(sqLiteDatabase);
         SharedPreferencesHelper.updateNumRevision(mContext, 0);
     }
