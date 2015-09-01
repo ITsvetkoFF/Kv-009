fdffde65b93000a870a82ffc2d54d497858353a6
y.ridkous@gmail.com <y.ridkous@gmail.com>
2015-09-01 15:02:55 +0300

    pending

--- app/src/main/java/org/ecomap/android/app/data/EcoMapDBHelper.java
+++ app/src/main/java/org/ecomap/android/app/data/EcoMapDBHelper.java
@@ -16,7 +16,7 @@ import org.ecomap.android.app.utils.SharedPreferencesHelper;
 public class EcoMapDBHelper extends SQLiteOpenHelper {
 
     // If you change the database schema, you must increment the database version.
-    private static final int DATABASE_VERSION = 2;
+    private static final int DATABASE_VERSION = 1;
 
     public static final String DATABASE_NAME = "ecomap.db";
     private final Context mContext;
